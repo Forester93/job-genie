99b1300fe3c419b8a37ae9258f692bb8c891192a
Marco <markforester93@gmail.com>
2021-04-12 23:01:13 +1000

    Backend API Zomato Functionality and Results Page Building

--- assets/scripts/results.js
+++ assets/scripts/results.js
@@ -1,8 +1,68 @@
-$(document).ready(function() {
-    //Read your query param
-    //Do fetch 
-    //Get JSON back
+let resultsSection=$('#results-section');
+let results= new URLSearchParams(window.location.href);
 
-    //Loop the results
-    //Create the cards dynamically 
-});
+let resultLat=results.get('latitude');
+let resultLong=results.get('long');
+let cuisineId=results.get('id');
+
+function buildPage(data){
+
+    for(let i=1;i<data.restaurants.length;i++){
+        let restaurant=$('<section>')
+            .addClass('card col-5 m-1 bg-primary text-dark')
+            .css("width","18rem")
+            .append('<br>')
+            .append('<br>')
+            .append('<br>')
+            .append($('<h2>')
+                .text(data.restaurants[i].restaurant.name)
+            )
+            .append($('<img>')
+                .addClass('card col-5-img-top restaurant-image')
+                .attr('src','./assets/images/sample.jpg')
+                .attr('alt','Photo of the restaurant')
+            )
+            .append($('<article>')
+                .addClass('card col-5-text p-5')
+                .append($('<p>')
+                    .addClass('card-text text-dark font-bold')
+                    .text(data.restaurants[i].restaurant.location.address))
+            )
+            .append($('<span>')
+                .addClass('card col-5-body cuisines')
+                .text("Cuisines Offered:"+data.restaurants[i].restaurant.cuisines)
+            )
+            .append($('<form>')
+                .addClass('row justify-content-center align-items-center')
+                .attr('restaurant-id',i)
+                .append($('<button>')
+                    .addClass('col-5 btn-success')
+                    .text('Get There')
+                )
+            );
+
+            resultsSection.append(restaurant);
+    }
+}
+
+// alert(resultLat);
+// alert(resultLong);
+// alert(cuisineId);
+
+fetch('https://developers.zomato.com/api/v2.1/search?entity_type=city&lat='+resultLat +'&lon='+resultLong+'&cuisine='+cuisineId+'&count=6' +'&sort=real_distance', {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+  headers: {
+  'user-key': '7749b19667964b87a3efc739e254ada2',
+  'accept': 'application/json'
+  }
+  
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.restaurants);
+    buildPage(data);
+
+  });
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,13 +4,28 @@ const searchButton = $("#searchBtn");
 const clearBtn = $("#clear");
 
 
+
 function handleSubmit(e) {
     e.preventDefault();
     alert(locationInputEl.value);
-    window.location.href = "./results.html?q=" + locationInputEl.value;
+
+    // localStorage.setItem(JSON.stringify(/*put cuisine id and lat and long object here */));
+    window.location.href = "./results.html?q=0&latitude=" + locationInputEl.getAttribute('data-lat') +'&long='+locationInputEl.getAttribute('data-lon')+'&id='+'100';
 }
 
 formEl.addEventListener("submit", handleSubmit)
 
 searchButton.on("click", searchButtonClicked);
 clearBtn.on("click",clearSearches);    
+
+
+// let lastSearched = {
+//     lat: '155',
+//     long: '22',
+//     cuisineId:'144'
+// }
+
+// function returnLastSearch(){
+
+//     let lastSearchParams= JSON.parse( localStorage.getItem(''))
+// }
