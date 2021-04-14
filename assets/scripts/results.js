e9fc0c76bff491446e425b7f6c240bb7b7a63454
Marco <markforester93@gmail.com>
2021-04-15 01:16:57 +1000

    Job Search Functionality Across the Application

--- assets/scripts/results.js
+++ assets/scripts/results.js
@@ -5,16 +5,14 @@ function buildPage(data){
     for(let i in data){
         let job=
                 $('<div>')
-                .addClass('col-md-6 my-2')
+                .addClass('col-md-9 my-2 border')
                 .append($('<div>')
                     .addClass('front-imgblock')
                     .append($('<div>')
                         .addClass('front-img')
                         .append($('<a>')
                             .attr('href',data[i].company_url)
-                            .append($('<img>')
-                                .attr('src','./assets/images/sample.jpg')
-                                .attr('alt','Results Image')
+                            .text(data[i].company)
                             )
                         )                        
                     )
@@ -23,16 +21,12 @@ function buildPage(data){
                         .append($('<h4>')
                             .text(data[i].title)
                         )
-                        .append($('<p>')
-                            .text(data[i].description)
-                        )
-
+                        .html(data[i].description)
                         .append($('<span>')
                             .addClass('card col-5-body cuisines')
                             .text("Type:"+data[i].type)
                         )
                     )
-                )
 
             resultsSection.append(job);
     }
@@ -46,23 +40,27 @@ const resultLong= data.longitude;
 const jobDescription = data.jobDescription;
 
 // alert('URL CONSTRUCTED (LAT & LON FROM DATASTORE): https://developers.zomato.com/api/v2.1/search?entity_type=city&lat='+resultLat +'&lon='+resultLong+'&cuisine='+cuisineId+'&count=6' +'&sort=real_distance');
+let url='';
+if (jobDescription=='All Programming Jobs'){
+    url='https://jobs.github.com/positions.json?lat='+resultLat+'&long='+resultLong;
+}else{
+    url='https://jobs.github.com/positions.json?description='+jobDescription+'lat='+resultLat+'&long='+resultLong;
+}
 
+alert(url);
 
-fetch('https://jobs.github.com/positions.json?lat='+resultLat+'&long='+resultLong
-,{
-  // The browser fetches the resource from the remote server without first looking in the cache.
-  // The browser will then update the cache with the downloaded resource.
-  headers: {
-    'Access-Control-Allow-Origin': 'https://forester93.github.io'
-  }
-  
-} 
+fetch(`https://api.codetabs.com/v1/proxy?quest=${url}`
+// ,{
+//   // The browser fetches the resource from the remote server without first looking in the cache.
+//   // The browser will then update the cache with the downloaded resource.
+// //   mode: "no-cors"
+// } 
 )
   .then(function (response) {
+    console.log(response);
     return response.json();
   })
   .then(function (data) {
-    console.log(data.restaurants);
     buildPage(data);
 
   });
